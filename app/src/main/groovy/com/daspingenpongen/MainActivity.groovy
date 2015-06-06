df5f862a0c14f6b0c006b0c0acc269427cac8926
Ordon <ordon1313@gmail.com>
2015-06-06 09:28:15 +0200

    @CompileStatic!

--- app/src/main/groovy/com/daspingenpongen/MainActivity.groovy
+++ app/src/main/groovy/com/daspingenpongen/MainActivity.groovy
@@ -3,9 +3,10 @@ package com.daspingenpongen;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuItem
+import groovy.transform.CompileStatic;
 
-
+@CompileStatic
 public class MainActivity extends ActionBarActivity {
 
     @Override
