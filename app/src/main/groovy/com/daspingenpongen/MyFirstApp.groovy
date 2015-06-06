718206dc7cded1bfca97f6f665d937d5eac72339
Ordon <ordon1313@gmail.com>
2015-06-06 09:24:55 +0200

    Relay app

---
+++ app/src/main/groovy/com/daspingenpongen/MyFirstApp.groovy
@@ -0,0 +1,14 @@
+package com.daspingenpongen
+
+import android.app.Application
+import groovy.transform.CompileStatic
+import io.relayr.RelayrSdk
+
+@CompileStatic
+final class MyFirstApp extends Application {
+    @Override
+    void onCreate() {
+        super.onCreate()
+        new RelayrSdk.Builder(this).inMockMode(false).build()
+    }
+}
